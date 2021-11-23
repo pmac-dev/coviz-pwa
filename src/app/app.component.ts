c0c3a5fc2c0a3cf8f448821a11057cebf3f1b34e
pmac-dev <63117811+pmac-dev@users.noreply.github.com>
2021-11-23 17:38:25 +0100

    Changed README.md
    Changed README.md and some icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+
+const URL = 'https://coviz.io' ;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coviz';
+
+
+  safeUrl: any ;
+
+  constructor(private domSanit: DomSanitizer) {
+    this.safeUrl = this.getSafeUrl() ;
+  }
+
+  getSafeUrl(){
+    return this.domSanit.bypassSecurityTrustResourceUrl(URL) ;
+  }
+
+  ngOnInit(): void {
+    this.safeUrl = this.getSafeUrl() ;
+  }
+
+ 
+
+
 }
